1568222bac0f46cbf742f4c6cb99349c77c86e33
prasadravi <prasadravi9866@gmail.com>
2025-12-06 09:47:45 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String args[]){
+        String name ="Ravi Prasad";
+        int age = 21;
+        System.out.println(name);
+    }
+    
+    
+}
