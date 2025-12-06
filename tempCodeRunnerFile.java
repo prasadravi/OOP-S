f7d3d9af732ccbafffc6ade9a0a6c01476e31e11
prasadravi <prasadravi9866@gmail.com>
2025-12-06 09:14:25 +0530

    oddnumber

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -17,7 +17,7 @@ import java.util.Scanner;
 //     }
 // }
 
-     class oddnumber{
+    public class oddnumber{
         public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
