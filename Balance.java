5e1aaee13fd80e293c8a231c4e24e24958c04300
prasadravi <prasadravi9866@gmail.com>
2025-12-07 10:14:27 +0530

    Balance

---
+++ Balance.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Balance{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+
+        if(n%2==0){
+            System.out.println("Not balanced");
+        }
+        else{
+            int mid=n / 2;
+            int ls=0,rs=0;
+            for(int i=0;i<=mid-1;i++){
+                ls+=s.charAt(i)-'0';
+
+            }
+            for(int i=mid+1;i < n;i++){
+                rs+=s.charAt(i)-'0';
+            }
+            if(ls==rs){
+                System.out.println("Balanced");
+            }
+            else{
+                System.out.println("Not Balanced");
+            }
+        }
+        
+    }
+}
+    
+
